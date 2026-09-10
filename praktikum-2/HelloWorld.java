7284c1992824bb9d0238f9c0621e24551153bc61
Cut Arsyi <cutarsyi3108@gail.com>
2026-09-10 14:49:18 +0700

    Praktikum 2: Struktur Dasar Program JavaA.2.

--- praktikum-2/HelloWorld.java
+++ praktikum-2/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Halo, Arsyi!");
+
+        System.out.print("Nama: ");
+        System.out.print("Arsyi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
