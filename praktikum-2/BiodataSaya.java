a37efc5199262e917f6f36220f74e0fb90166c50
Cut Arsyi <cutarsyi3108@gail.com>
2026-09-10 16:36:57 +0700

    Praktikum 2: Latihan 2

--- praktikum-2/BiodataSaya.java
+++ praktikum-2/BiodataSaya.java
@@ -2,9 +2,15 @@ public class BiodataSaya {
     public static void main(String[] args) {
         // Menampilkan nama lengkap mahasiswa
         System.out.println("Nama: Cut Arsyi");
+
+        // Menampilkan Nama dan NIM pada baris yang sama, dipisah " - "
+        System.out.print("Nama: Cut Arsyi");
+        System.out.print("-");
         // Menampilkan NIM mahasiswa
         System.out.println("NIM: 2025573010072");
+
+        System.out.print("NIM: 2025573010072");
+        System.out.println(); //untuk berpindah baris
         // Menampilkan program studi mahasiswa
         System.out.println("Program Studi: Teknik Informatika");
     }
-}
