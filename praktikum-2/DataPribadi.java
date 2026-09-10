a449bac49806398996ed82d2e71b3739f0aa2430
Cut Arsyi <cutarsyi3108@gail.com>
2026-09-10 15:30:21 +0700

    Praktikum 2: Latihan 3

---
+++ praktikum-2/DataPribadi.java
@@ -0,0 +1,15 @@
+public class DataPribadi {
+    public static void main(String[] args) {
+        // Deklarasi variabel data pribadi
+        int umur = 19;                  // tipe int
+        double tinggiBadan = 155.5;     // tipe double
+        char inisial = 'A';             // tipe char
+        boolean mahasiswaAktif = true;  // tipe boolean
+
+        // Cetak seluruh variabel
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Tinggi Badan: " + tinggiBadan + " cm");
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Mahasiswa Aktif: " + mahasiswaAktif);
+    }
+}
